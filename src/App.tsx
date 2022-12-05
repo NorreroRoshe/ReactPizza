c6cb829515f046e1575b4502d696622a04ec9539
NorreroRoshe <norikas95@mail.ru>
2022-12-05 11:01:58 +0300

    asds

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         <Route path="" element={<Home />} />
         <Route path="cart" element={<Cart />} />
         <Route path="pizza/:id" element={<FullPizza />} />
-        <Route path="/:ReactPizza" element={<Home />} />
+        <Route path="/:ReactPizza/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
